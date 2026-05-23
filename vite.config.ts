8fe21f7bd4d84744dcff262e28add1bbdb03f247
Boorlu Guna sekhar <gunasekharboorlu@gmail.com>
2026-05-23 11:37:12 +0530

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Add this base path for GitHub Pages deployment
+  base: '/santex-Smart-Interoperable-Healthcare-Record-Management-System/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
